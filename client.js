e577dd1f359a4639c45d60845c857bd3410da416
Victor Ermolaev <victorermolaev@gmail.com>
2024-10-16 11:17:04 +0200

    new

--- client.js
+++ client.js
@@ -586,8 +586,8 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper520 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 206, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper518 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 204, __wbg_adapter_24);
         return addHeapObject(ret);
     };
 
